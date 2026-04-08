9145a4c8e76ae1b70b1019fe77f9615de636eb22
Fabian Esteban Guayacan Neva <fguayacan@ucompensar.edu.co>
2026-04-07 19:52:50 -0500

    Agregué RecyclerView y navegación funcional

--- app/src/main/java/com/example/taller2_fabian/iu/main/MainActivity.kt
+++ app/src/main/java/com/example/taller2_fabian/iu/main/MainActivity.kt
@@ -2,14 +2,17 @@ package com.example.taller2_fabian.iu.main
 
 import android.os.Bundle
 import android.widget.ImageView
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.example.taller2_fabian.R
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var drawerLayout: androidx.drawerlayout.widget.DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,5 +26,24 @@ class MainActivity : AppCompatActivity() {
         btnMenu.setOnClickListener {
             drawerLayout.openDrawer(GravityCompat.START)
         }
+
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        val navView = findViewById<NavigationView>(R.id.nav_view)
+
+        setSupportActionBar(toolbar)
+
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
     }
 }
