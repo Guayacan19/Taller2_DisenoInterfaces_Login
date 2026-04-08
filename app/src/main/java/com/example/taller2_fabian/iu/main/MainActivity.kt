1771f9e14a168ae5281e81d25583388a8ab453cb
Fabian Esteban Guayacan Neva <fguayacan@ucompensar.edu.co>
2026-04-07 21:00:13 -0500

    Arreglos en Main y navegación

--- app/src/main/java/com/example/taller2_fabian/iu/main/MainActivity.kt
+++ app/src/main/java/com/example/taller2_fabian/iu/main/MainActivity.kt
@@ -8,11 +8,13 @@ import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.example.taller2_fabian.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var drawerLayout: androidx.drawerlayout.widget.DrawerLayout
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,20 +22,17 @@ class MainActivity : AppCompatActivity() {
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+        drawerLayout = findViewById(R.id.drawer_layout)
         val btnMenu = findViewById<ImageView>(R.id.btnMenu)
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        val navView = findViewById<NavigationView>(R.id.nav_view)
 
+        // Botón menú
         btnMenu.setOnClickListener {
             drawerLayout.openDrawer(GravityCompat.START)
         }
 
-        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
-        drawerLayout = findViewById(R.id.drawer_layout)
-        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
-        val navView = findViewById<NavigationView>(R.id.nav_view)
-
-        setSupportActionBar(toolbar)
-
+        // Drawer toggle
         val toggle = ActionBarDrawerToggle(
             this,
             drawerLayout,
@@ -44,6 +43,5 @@ class MainActivity : AppCompatActivity() {
 
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
-
     }
 }
